987b22c2183b15b7a9ef420cbb7583ee0eea45b7
Adolfo Griego <104970935+AdolfoGriego@users.noreply.github.com>
2023-05-12 17:12:55 -0600

    Add files via upload

---
+++ plugins/info-creador.js
@@ -0,0 +1,39 @@
+let handler = async (m, { conn, usedPrefix }) => {
+var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
+var document = doc[Math.floor(Math.random() * doc.length)]    
+let text = `
+*• https://atom.bio/adolfogriego
+`.trim()   
+let buttonMessage= {
+'document': { url: `https://github.com/AdolfoGriego/BakiBot-MD` },
+'mimetype': `application/${document}`,
+'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
+'fileLength': 99999999999999,
+'pageCount': 200,
+'contextInfo': {
+'forwardingScore': 200,
+'isForwarded': true,
+'externalAdReply': {
+'mediaUrl': 'https://github.com/AdolfoGriego/BakiBot-MD',
+'mediaType': 2,
+'previewType': 'pdf',
+'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
+'body': wm,
+'thumbnail': imagen1,
+'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA' }},
+'caption': text,
+'footer': wm,
+'buttons':[
+{buttonId: `${usedPrefix}menu`, buttonText: {displayText: 'MENU'}, type: 1}, 
+{buttonId: `${usedPrefix}donar`, buttonText: {displayText: 'APOYAR'}, type: 1}],
+'headerType': 6 }
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+//let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;AdolfoGriego;;;\nFN:AdolfoGriego\nORG:AdolfoGriego\nTITLE:\nitem1.TEL;waid=5219996125657:+521 999 612 5657\nitem1.X-ABLabel:AdolfoGriego\nX-WA-BIZ-DESCRIPTION:[❗] ᴄᴏɴᴛᴀᴄᴛᴀ ᴀ ᴇsᴛᴇ ɴᴜᴍ ᴘᴀʀᴀ ᴄᴏsᴀs ɪᴍᴘᴏʀᴛᴀɴᴛᴇs.\nX-WA-BIZ-NAME:AdolfoGriego\nEND:VCARD`
+//await conn.sendMessage(m.chat, { contacts: { displayName: 'AdolfoGriego', contacts: [{ vcard }] }}, {quoted: m})
+//const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+//await conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+handler.command = /^(owner|creator|creador|propietario)$/i
+export default handler
